8e21df0439bb2858c7af106469ee92ad37e6c248
juanpablo1399 <juanpablo9913@outlook.com>
2022-04-29 19:32:37 -0500

    codigoparcial2

--- parcial.py
+++ parcial.py
@@ -1,2 +1,27 @@
-#estamos embalados
+#codigo del parcial
+#escribir un codigo para invertir un array de string, por ejemplo su la entrada es ["1","2","3","4","1"]. el codigo debe cumplir con los sigueintes paearmetros 
+#1. se debe solicitar la cantidad de elementos del array 
+#2. se debe solicitar por pantalla el valor de cada uno de los elementos 
+#3. se debe crear una funcion que reciba el array y retorne el array invertido.
+#4. se debe mostrar en pantalla el array original y el invertido 
+
+
+#Desarollo 
+
+
+#creamos el array 
+
+from multiprocessing.dummy import Array
+
+
+arregloparcial=["1","2","3","4","1"]
+print("la lista es ",arregloparcial)
+
+#reversamos el array
+
+arregloparcial.reverse(["1","2","3","4","1"])
+print("el arreglo invertido es",arregloparcial.reverse)
+
+#profe no supe arerglar ese error no lo entiendo
+
 
